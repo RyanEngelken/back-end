4023709d5ed7c520c31a1e42b95e6fc832db29f0
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-13 13:39:43 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//setup .. this is similar to when we use our default tags in html
+const express = require('express');
+//activate or tell this app varibale to be an express server
+const app = express(); 
+
+const router = express.Router(); 
+
+
+//star the web server ..  app.listen(portnumber, function)
+app.listen(3000, function() {
+    console.log("Listening on port 3000")
+});
+
+//making an api using routes
+//routes are used to handle browswer requests. The look like urls, the difference is that when a browswer request a route it is dynamically handled by using a function. 
+
+
+//Get or a regular request when someone goes to http://localhost:3000/hello
+//local development is localhost:3000, but when you deploy it to a server it will be the server address
+//when using a function in a route we almost always have a paramater or handle a response and request
+
+/* app.get('/hello', function(req, res) {
+    res.send("<h1>Hello Express</h1>");
+});
+
+app.get('/goodbye', function(req, res) {
+    res.send("<h1>Goodbye, Express!</h1>");
+}); */
+
+

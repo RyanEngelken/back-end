7f6461fb8b3c544a0f9d16021cee6586e56f68c1
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-13 14:11:44 -0400

    created first api route to get sons

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 //setup .. this is similar to when we use our default tags in html
 const express = require('express');
+//we have to use cors to host front end and backend on same device
+var cors = require('cors')
 //activate or tell this app varibale to be an express server
-const app = express(); // create an instance of express
+const app = express(); 
+app.use(cors())
+const router = express.Router(); 
 
 
-//star the web server ..  app.listen(portnumber, function)
-app.listen(3000, function() {
-    console.log("Listening on port 3000")
-});
 
 //making an api using routes
 //routes are used to handle browswer requests. The look like urls, the difference is that when a browswer request a route it is dynamically handled by using a function. 
 
-
-//Get or a regular request when someone goes to http://localhost:3000/hello
-//local development is localhost:3000, but when you deploy it to a server it will be the server address
-//when using a function in a route we almost always have a paramater or handle a response and request
-app.get('/hello', function(req, res) {
-    res.send("<h1>Hello Express</h1>");
+router.get("/songs", function(req, res) {
+    const songs = [ 
+        {
+        title: "We Found Love",
+        artist: "Rihanna",
+        popularity: 10,
+        releaseDate: new Date(2011, 9, 22),
+        genre: ["electro house"]
+    },
+    {
+        title: "Happy",
+        artist: "Pharrell Williams",
+        popularity: 10,
+        releaseDate: new Date(2011, 9, 22),
+        genre: ["soul", "new soul"]
+    } ]
+    //this is transforming the song object into a json string and sending it to the client
+    res.json(songs)
 });
 
-app.get('/goodbye', function(req, res) {
-    res.send("<h1>Goodbye, Express!</h1>");
-});
+//all requests that usually use an api start with /api... so the url would be localhost:3000/api/songs
+//this says before using anything else, use this router first
+app.use("/api", router);
+app.listen(3000);
